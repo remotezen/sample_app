b8e528f860f41b7a84a26aeeca4d97347bad996b
remotezen <remotezen@bell.net>
2017-07-28 12:02:50 -0400

    all tests pass

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -34,6 +34,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+
   test "email validation should accept valid format" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
@@ -75,4 +76,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with a nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
